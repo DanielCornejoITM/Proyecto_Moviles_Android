b1f4f429db82815cec1fe22babde2f4806220588
Daniel Cornejo <danielcornejo1994@hotmail.com>
2018-06-04 16:06:15 -0500

    Añadido nuevo Manejador de Aplicacion de facebook y Activity de Catalogo y Sugerencias

--- Neto_Proyecto/Juegos_Final/app/src/main/java/com/example/ernesto/juegos_final/CarritoActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.example.ernesto.juegos_final
-
-class CarritoActivity {
-}
--- Neto_Proyecto/Juegos_Final/app/src/main/java/com/example/ernesto/juegos_final/Inicio.kt
+++ Neto_Proyecto/Juegos_Final/app/src/main/java/com/example/ernesto/juegos_final/Inicio.kt
@@ -1,5 +1,6 @@
 package com.example.ernesto.juegos_final
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -14,13 +15,24 @@ class Inicio : AppCompatActivity() {
         setContentView(R.layout.activity_inicio)
 
         var fermer = findViewById<Button>(R.id.Cerrar)
+        var Catalogo = findViewById<Button>(R.id.BotonCata)
+        var Sugerencias= findViewById<Button>(R.id.BotonSug)
+
+
+
         fermer.setOnClickListener(View.OnClickListener {
 
             Toast.makeText(applicationContext, "Cerrando Sesion", Toast.LENGTH_LONG).show()
             LoginManager.getInstance().logOut()
             finish()
+        })
+        Catalogo.setOnClickListener(View.OnClickListener {
 
+            startActivity(Intent(applicationContext,com.example.ernesto.juegos_final.Catalogo::class.java))
 
+        })
+        Sugerencias.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(applicationContext,com.example.ernesto.juegos_final.Sugerencias::class.java))
 
         })
 
