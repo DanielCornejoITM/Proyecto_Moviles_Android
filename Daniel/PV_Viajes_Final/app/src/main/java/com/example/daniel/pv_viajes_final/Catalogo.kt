95d93508a76b855bb1c419ebc574b41339393c77
Daniel Cornejo <danielcornejo1994@hotmail.com>
2018-06-05 18:31:23 -0500

    Se Agrego ListView

--- Daniel/PV_Viajes_Final/app/src/main/java/com/example/daniel/pv_viajes_final/Catalogo.kt
+++ Daniel/PV_Viajes_Final/app/src/main/java/com/example/daniel/pv_viajes_final/Catalogo.kt
@@ -1,12 +1,68 @@
 package com.example.daniel.pv_viajes_final
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 
-class Catalogo : AppCompatActivity() {
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_catalogo.*
+import kotlinx.android.synthetic.main.activity_sugerencias.view.*
+import kotlinx.android.synthetic.main.modecatalogo.view.*
 
+class Catalogo(var adaptador:ProductosAdaptador?=null) : AppCompatActivity() {
+
+    var listaCatalogo = ArrayList<Productos>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_catalogo)
+
+        listaCatalogo.add(Productos(30.0F,"Amigo"))
+        listaCatalogo.add(Productos(20.0F,"Cosa"))
+        listaCatalogo.add(Productos(10.0F,"Memo"))
+        listaCatalogo.add(Productos(30.5F,"Hola"))
+        listaCatalogo.add(Productos(330.0F,"Sociedad"))
+        listaCatalogo.add(Productos(80.0F,"Perro"))
+        listaCatalogo.add(Productos(110.0F,"Gustavo"))
+        listaCatalogo.add(Productos(330.0F,"Nieto"))
+        listaCatalogo.add(Productos(900.0F,"Momo"))
+        adaptador =ProductosAdaptador(this,this.listaCatalogo)
+
+
+        Milista.adapter=adaptador
+    }
+
+    class ProductosAdaptador(contexto:Context,var ListaProductos: ArrayList<Productos>):BaseAdapter(){
+       var contexto1:Context?=contexto
+
+
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val producto=ListaProductos[position]
+            val inflador= contexto1!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val mivista =inflador.inflate(R.layout.modecatalogo,null)
+            mivista.Titulo.text=producto.Titulo!!
+            mivista.Costo.text=producto.Precio.toString()!!
+            return mivista
+
+        }
+
+        override fun getItem(position: Int): Any {
+            return ListaProductos[position]
+
+        }
+
+        override fun getItemId(position: Int): Long {
+            return  position.toLong()
+        }
+
+        override fun getCount(): Int {
+
+            return ListaProductos.size
+        }
+
+
     }
 }
---
+++ Daniel/PV_Viajes_Final/app/src/main/java/com/example/daniel/pv_viajes_final/Productos.kt
@@ -0,0 +1,16 @@
+package com.example.daniel.pv_viajes_final
+
+class Productos {
+    var Titulo :String?=null
+    var Precio : Float?=null
+
+
+constructor(precio:Float,titulo:String ){
+    this.Precio=precio
+    this.Titulo=titulo
+
+
+
+
+}
+}
